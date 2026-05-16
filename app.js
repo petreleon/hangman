666b7c8429e15de4a9f620f800fd13c5b8821395
Petre Leonard Macamete <petreleonardos@gmail.com>
2026-05-16 22:38:13 +0300

    Refactor Game class to include health management and update health display

--- app.js
+++ app.js
@@ -3,13 +3,45 @@ let word_list;
 let chosen_word;
 
 class Game {
-  constructor(word, word_element, on_succeed, on_fail) {
+  constructor(word, word_element, health_element, on_succeed, on_fail) {
     this.word = word;
     this.element = word_element;
+    this.health_element = health_element
     this.health = 7;
     this.on_succeed = on_succeed;
     this.on_fail = on_fail;
     this.element.innerHTML = "_" * this.word.length;
+    this.already_tried = "";
+  }
+  updateHealth() {
+    this.health_element.innerHTML = this.health.toString();
+  }
+  tryLetter(letter) {
+    if (this.isAlpha(letter) && !this.already_tried.includes(letter)) {
+      let indices = [];
+      this.already_tried += letter;
+      for(let iter = 0; iter < str.length; iter++) {
+        if (str[iter] === letter) indices.push(iter);
+      }
+      if (indices.length == 0) {
+        this.health -= 1;
+        this.updateHealth();
+        if (this.health == 0) {
+          this.on_fail()
+        }
+      }
+      if (indices.length != 0) {
+        split_string = this.element.innerHTML.split();
+        for (let indic of indices) {
+          [indic];
+        }
+      }
+
+    }
+  }
+  isAlpha(letter){
+    return typeof letter === "string" && letter.length === 1
+      && (letter >= "a" && letter <= "z" || letter >= "A" && letter <= "Z");
   }
 }
 
