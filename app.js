9a95c44ca68dc3602b3e3f692d233013ed82fa64
Petre Leonard Macamete <petreleonardos@gmail.com>
2026-05-21 11:12:01 +0300

    corercion

--- app.js
+++ app.js
@@ -14,24 +14,24 @@ class UI {
     this.health_element = health_element;
     this.reset_button = reset_button;
   }
-
+  
   empty(number) {
     this.word_element.innerHTML = "_".repeat(number);
   }
-
+  
   introduceLetter(letter, indexes) {
     let split_string = this.word_element.innerHTML.split("");
     for (let index of indexes) {
-      console.log(index)
+      console.log(index);
       split_string[index] = letter;
     }
     this.word_element.innerHTML = split_string.join("");
   }
-
+  
   updateHealth(number) {
     this.health_element.innerHTML = number.toString()
   }
-
+  
   assignReset(resetFunction) {
     this.reset_button.addEventListener('click', resetFunction);
   }
@@ -39,26 +39,32 @@ class UI {
 
 class GamePlay {
   constructor(word, _UI) {
+    this.health = 7;
     this._UI = _UI;
     this.word = word;
     this.word_map = {};
-    for (let index = 0; index < this.word_length; index += 1) {
+    for (let index = 0; index < this.word.length; index += 1) {
       if (!this.word_map.hasOwnProperty(word[index])) {
-        this.word_map[index] = [];
+        this.word_map[word[index]] = [];
       }
       this.word_map[word[index]].push(index);
     }
     this.already_tried = [];
     this.correct_letters = [];
+    this._UI.empty(word.length);
     this.updateHealth();
   }
-
+  
+  onSucceed() {
+    alert("Game Won!");
+  }
+  
   updateHealth() {
     this._UI.updateHealth(this.health);
   }
 
   isVictory() {
-    return this.correct_letters.length == this.word.length;
+    return this.correct_letters.length == Object.keys(this.word_map).length;
   }
 
   wrongLetter() {
@@ -70,33 +76,34 @@ class GamePlay {
   }
 
   correctLetter(letter, indexes) {
-    this._UI.introduceLetter
+    this._UI.introduceLetter(letter, indexes);
   }
 
   onFail() {
-    alert()
+    alert("Game over! Word was " + this.word);
   }
 
   tryLetter(letter) {
     if (this.health == 0) {
-      this.on_fail()
+      this.onFail()
       return;
     }
-    if (!this.element.innerHTML.includes("_")) {
-      this.on_succeed();
+    if (this.isVictory()) {
+      this.onSucceed();
       return;
     }
     if (Utils.isAlpha(letter) && !this.already_tried.includes(letter)) {
-      let indices = [];
       this.already_tried.push(letter);
-      for (let iter = 0; iter < this.word_map.length; iter++) {
-        if (this.word_map[iter] === letter) indices.push(iter);
-      }
-      if (indices.length == 0) {
+
+      if (!this.word_map.hasOwnProperty(letter)) {
         this.wrongLetter();
       }
-      if (indices.length != 0) {
-        this.correctLetter(letter, indices);
+      if (this.word_map.hasOwnProperty(letter)) {
+        this.correctLetter(letter, this.word_map[letter]);
+        this.correct_letters.push(letter);
+        if (this.isVictory()) {
+          this.onSucceed();
+        }
       }
     }
   }
@@ -106,35 +113,35 @@ class GamePlay {
 
 class Game {
   constructor(words, _UI) {
-    this.words = words;
+    this.words = [...words];
     this._UI = _UI;
     this._UI.assignReset(this.newGame);
     this.newGame();
   }
 
   newGame() {
-    let word = words[Math.floor(Math.random() * words.length)];
+    let word = this.words[Math.floor(Math.random() * this.words.length)];
     this.game_play = new GamePlay(word, this._UI);
   }
 }
 
 document.addEventListener("DOMContentLoaded",
   function () {
-    word_element = document.getElementById("word");
-    health_element = document.getElementById("health");
-    reset_button = document.getElementById("reset");
-    won = () => alert("Game Won!");
-    over = () => alert("Game Over!");
+    let word_element = document.getElementById("word");
+    let health_element = document.getElementById("health");
+    let reset_button = document.getElementById("reset");
+
     fetch("words.txt")
       .then((res) => res.text())
       .then((text) => {
         // Do something with "text"
-        word_list = text.toUpperCase().split("\n");
-        word = GameInitializer.newRandomGame(word_list, word_element, health_element, won, over);
+        let word_list = text.toUpperCase().split("\n");
+        let _UI = new UI(word_element, health_element, reset_button);
+        game = new Game(word_list, _UI);
       })
       .catch((e) => console.error(e));
     document.addEventListener('keydown', (event) => {
       console.log(`You pressed: ${event.key}`);
-      word.tryLetter(event.key.toUpperCase());
+      game.game_play.tryLetter(event.key.toUpperCase());
     });
   })
